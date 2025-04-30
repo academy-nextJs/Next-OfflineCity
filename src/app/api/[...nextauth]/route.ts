95d6a3ca38cc624360cb13afa7a3769b16a22e0d
malja1500 <mianroudianf@gmail.com>
2025-04-30 12:56:46 +0330

    add route

---
+++ src/app/api/[...nextauth]/route.ts
@@ -0,0 +1,42 @@
+import NextAuth from "next-auth";
+import CredentialsProvider from "next-auth/providers/credentials";
+
+const handler = NextAuth({
+  providers: [
+    CredentialsProvider({
+      name: "credentials",
+      credentials: {
+        email: { label: "ایمیل", type: "text" },
+        password: { label: "رمز عبور", type: "password" },
+      },
+      async authorize(credentials) {
+        const res = await fetch(
+          "https://delta-project.liara.run/api/auth/login",
+          {
+            method: "POST",
+            headers: { "Content-Type": "application/json" },
+            body: JSON.stringify({
+              email: credentials?.email,
+              password: credentials?.password,
+            }),
+          }
+        );
+
+        const user = await res.json();
+
+        if (!res.ok || !user) return null;
+
+        return user;
+      },
+    }),
+  ],
+  pages: {
+    signIn: "/login",
+  },
+  session: {
+    strategy: "jwt",
+  },
+  secret: process.env.NEXTAUTH_SECRET,
+});
+
+export { handler as GET, handler as POST };

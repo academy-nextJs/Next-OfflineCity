45d9d0c821c0ed4ba6a0c15526888ce2913ba8df
malja1500 <mianroudianf@gmail.com>
2025-05-01 15:29:24 +0330

    toast added

--- src/app/providers.tsx
+++ src/app/providers.tsx
@@ -1,13 +1,21 @@
 "use client";
 
 import { HeroUIProvider } from "@heroui/react";
+import { SessionProvider } from "next-auth/react";
 import { Provider } from "react-redux";
+import { ToastContainer } from "react-toastify";
 import store from "@/redux/store";
+import "react-toastify/dist/ReactToastify.css";
 
 export function Providers({ children }: { children: React.ReactNode }) {
   return (
-    <HeroUIProvider>
-      <Provider store={store}>{children}</Provider>
-    </HeroUIProvider>
+    <SessionProvider>
+      <HeroUIProvider>
+        <Provider store={store}>
+          {children}
+          <ToastContainer autoClose={3000} />
+        </Provider>
+      </HeroUIProvider>
+    </SessionProvider>
   );
 }

1d9e41275599017a196c3a3e9c84f3885384ef13
saeed <saeied78kh@gmail.com>
2025-05-03 20:25:01 +0330

    login form update

--- src/components/auth/LoginHeader.tsx
+++ src/components/auth/LoginHeader.tsx
@@ -11,7 +11,7 @@ export default function LoginHeader() {
         <Link
           href="/"
           className="inline-flex items-center justify-between gap-2 rounded-full px-4 py-2.5 text-sm w-[135px] h-[40px]
-            bg-[#F0F0F0] text-black dark:bg-white/10 dark:text-white transition-colors"
+            bg-[#F0F0F0] text-black dark:bg-white/10 dark:text-white dark:border transition-colors"
         >
           صفحه اصلی
           <ArrowLeft className="w-4 h-4" />

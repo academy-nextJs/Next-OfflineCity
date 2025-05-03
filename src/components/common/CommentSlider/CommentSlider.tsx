a5f71e33087b9b913be5ea1fd8ac451481af9023
sina <poorprofessor2000@gmail.com>
2025-05-03 20:52:19 +0330

    fix comment slider

--- src/components/common/CommentSlider/CommentSlider.tsx
+++ src/components/common/CommentSlider/CommentSlider.tsx
@@ -1,35 +1,13 @@
 'use client'
 
-import { Navigation , Pagination  } from "swiper/modules";
+import { Navigation  } from "swiper/modules";
 import { Swiper , SwiperSlide } from "swiper/react";
 import 'swiper/css';
 import 'swiper/css/pagination';
 import 'swiper/css/navigation';
 import { JSX, useEffect, useRef, useState } from "react";
 import clsx from 'clsx'
-import{Swiper as SwiperType } from 'swiper'
 import axiosInstance from "@/utils/services/interceptor/axios";
-
-
-type commentsProps = {
-    id:number;
-    text:string;
-    author: string;
-    date:string
-}
-
-// const comments:commentsProps[] = [
-//    {id: 1 , text : 'لورم ایپسوم متن ساختگی با تولید سادگی نامفهوم   از صنعت چاپ، و با استفاده از طراحان گرافیک است، چاپگرها و متون بلکه روزنامه و مجله در ستون و سطرآنچنان که لازم است، و برای شرایط فعلی تکنولوژی مورد نیاز، و کاربردهای متنوع با هدف بهبود ابزارهای کاربردی می باشد، کتابهای زیادی در شصت و سه درصد گذشته حال و آینده، شناخت فراوان جامعه و متخصصان را می طلبد، تا با نرم افزارها شناخت بیشتری را برای طراحان رایانه ای علی الخصوص طراحان خلاقی، و فرهنگ پیشرو در زبان فارسی ایجاد کرد، در این صورت می توان امید داشت که تمام و دشواری موجود در ارائه راهکارها، و شرایط سخت تایپ به پایان رسد', author: 'پارسا' , date: "12,12,12"},
-//   //  {id: 1 , text : 'این یک پیام تستی هستش ولی بدئون خلق' , author: 'پارسا' , date: "12,12,12"},
-//   //  {id: 1 , text : 'این یک پیام تستی هستش ولی بدئون خلق' , author: 'پارسا' , date: "12,12,12"},   
-//   //  {id: 1 , text : 'این یک پیام تستی هستش ولی بدئون خلق' , author: 'پارسا' , date: "12,12,12"},
-//   //  {id: 1 , text : 'این یک پیام تستی هستش ولی بدئون خلق' , author: 'پارسا' , date: "12,12,12"},
-//   //  {id: 1 , text : 'این یک پیام تستی هستش ولی بدئون خلق' , author: 'پارسا' , date: "12,12,12"},   
-//   //  {id: 1 , text : 'این یک پیام تستی هستش ولی بدئون خلق' , author: 'پارسا' , date: "12,12,12"},
-//   //  {id: 1 , text : 'این یک پیام تستی هستش ولی بدئون خلق' , author: 'پارسا' , date: "12,12,12"},
-//   //  {id: 1 , text : 'این یک پیام تستی هستش ولی بدئون خلق' , author: 'پارسا' , date: "12,12,12"},   
-   
-// ]
 
 type dataProps = {
   title:string;
@@ -42,12 +20,9 @@ type dataProps = {
 const CommentsSlider: React.FC = () => {
 
     const [activeIndex, setActiveIndex] = useState<number>(0)
-    const swiperRef = useRef<SwiperType |null>(null)
+  
     const [data, setData] = useState<dataProps[]>()
 
-    // const [comments, setComments] = useState({
-    //   text: 'لورم ایپسوم متن ساختگی با تولید سادگی نامفهوم از صنعت چاپ، و با استفاده از طراحان گرافیک است، چاپگرها و متون بلکه روزنامه و مجله در ستون و سطرآنچنان که لازم است، و برای شرایط فعلی تکنولوژی مورد نیاز، و کاربردهای متنوع با هدف بهبود ابزارهای کاربردی می باشد، کتابهای زیادی در شصت و سه درصد گذشته حال و آینده، شناخت فراوان جامعه و متخصصان را می طلبد، تا با نرم افزارها شناخت بیشتری را برای طراحان رایانه ای علی الخصوص طراحان خلاقی، و فرهنگ پیشرو در زبان فارسی ایجاد کرد، در این صورت می توان امید داشت که تمام و دشواری موجود در ارائه راهکارها، و شرایط سخت تایپ به پایان رسد'
-    // })
  
 
     const GetData = async  () => {
@@ -91,18 +66,18 @@ const CommentsSlider: React.FC = () => {
         >
             {data?.map((comment:dataProps , index:number) => {
                 const diff = Math.abs(index - activeIndex);
-                let size = 'w-40 h-48 xl:w-[178px] xl:h-[197px] mr-12 opacity-25 mt-32';
+                let size = ' opacity-25 mt-32 space-y-10 ';
 
-                if (index === activeIndex) size = 'w-64 h-72 xl:w-[385px] xl:h-[426px] ';
+                if (index === activeIndex) size = 'space-y-60  ';
 
-                else if (diff === 1 || diff === data?.length - 1) size = 'w-52 h-64 xl:w-[279px] xl:h-[308px] mr-10 mt-16 '
+                else if (diff === 1 || diff === data?.length - 1) size = ' mt-16 space-y-40'
          
 
           return(
 
                 <SwiperSlide
                    key={comment.id}
-                   className="flex justify-center items-center pr-20 mt- "
+                   className="flex justify-center items-center pr-20  "
                 >   
                 <div
                   className={clsx(

54919907ee6c6eb9feedf8b82f965537963fff4f
saeed <saeied78kh@gmail.com>
2025-05-10 14:52:35 +0330

    fixed conflict

--- src/components/auth/signup/Step1Email.tsx
+++ src/components/auth/signup/Step1Email.tsx
@@ -3,7 +3,6 @@
 import { useState } from "react";
 import { toast } from "react-toastify";
 import AuthHeader from "../AuthHeader";
-import Link from "next/link";
 
 interface Step1EmailProps {
   onNext: () => void;
@@ -98,9 +97,9 @@ export default function Step1Email({ onNext }: Step1EmailProps) {
 
       <p className="text-center text-sm font-yekan mt-4 text-gray-600 dark:text-gray-300">
         حساب کاربری دارید؟{" "}
-        <Link href="/Login" className="text-primary font-bold hover:underline">
+        <a href="/login" className="text-primary font-bold hover:underline">
           ورود به حساب
-        </Link>
+        </a>
       </p>
     </form>
   );
